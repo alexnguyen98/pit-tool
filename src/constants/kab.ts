9fe513d1e7845a23368cfa06e444b29b862e3d56
alex.nguyen <alex.nguyen@applifting.cz>
2022-01-09 23:38:18 +0100

    update

--- src/constants/kab.ts
+++ src/constants/kab.ts
@@ -282,6 +282,256 @@ export const KAB_QUESTIONS: QuestionT[] = [
       },
       {
         text: 'všechny operace jsou stejně důležité',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    subject: SubjectType.KAB,
+    question:
+      'Mějme funkci T(n) = 4n^3 + 175n^2 + 2^n + 10n + 99999999, která popisuje časovou složitost řešení nějakého problému. Vyberte ekvivalentní zápis časové složitosti této funkce pomocí O notace (shora neostře ohraničená).',
+    img: null,
+    options: [
+      {
+        text: 'jiná správná odpověď',
+        correct: false,
+      },
+      {
+        text: 'O(1)',
+        correct: false,
+      },
+      {
+        text: 'O(n)',
+        correct: false,
+      },
+      {
+        text: 'O(n^3)',
+        correct: false,
+      },
+      {
+        text: 'O(n^2)',
+        correct: false,
+      },
+      {
+        text: 'O(2^n)',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 10,
+    subject: SubjectType.KAB,
+    question:
+      'Mějme funkci T(n) = 15n3 + 175n^2 + 20n + 100000, která popisuje časovou složitost řešení nějakého problému. Vyberte ekvivalentní zápis časové složitosti této funkce pomocí O notace (shora neostře ohraničená).',
+    img: null,
+    options: [
+      {
+        text: 'jiná správná odpověď',
+        correct: false,
+      },
+      {
+        text: 'O(1)',
+        correct: false,
+      },
+      {
+        text: 'O(n)',
+        correct: false,
+      },
+      {
+        text: 'O(n^3)',
+        correct: true,
+      },
+      {
+        text: 'O(n^2)',
+        correct: false,
+      },
+      {
+        text: 'O(10^n)',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    subject: SubjectType.KAB,
+    question: 'Elektronicky podepsat lze',
+    img: null,
+    options: [
+      {
+        text: 'všechny dokumenty MS Office od verze 2007',
+        correct: false,
+      },
+      {
+        text: 'pouze textové dokumenty fotmátu TXT',
+        correct: false,
+      },
+      {
+        text: 'pouze textové dokumenty fotmátu TXT',
+        correct: false,
+      },
+      {
+        text: 'pouze emaily bez přílohy (podle RFC 822)',
+        correct: false,
+      },
+      {
+        text: 'všechna digitální data',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 12,
+    subject: SubjectType.KAB,
+    question: 'Kryptografie',
+    img: null,
+    options: [
+      {
+        text: 'zkoumá odolnost a zranitelnost kryptosystémů',
+        correct: false,
+      },
+      {
+        text: 'je návrh a konstrukce kryptografických algoritmů a způsoby jejich využívání',
+        correct: true,
+      },
+      {
+        text: 'se zabývá metodami získáváni otevřeného textu z textu šifrového bez znalosti klíče',
+        correct: false,
+      },
+      {
+        text: 'není ani jedna z uvedených možností',
+        correct: false,
+      },
+      {
+        text: 'je vědní obor zahrnující kryptologii a kryptoanalýzu',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    subject: SubjectType.KAB,
+    question: 'Šifrování hovorů v GSM se děje pomocí algoritmu',
+    img: null,
+    options: [
+      {
+        text: 'E0',
+        correct: false,
+      },
+      {
+        text: 'A0',
+        correct: false,
+      },
+      {
+        text: 'RC4',
+        correct: false,
+      },
+      {
+        text: 'A3',
+        correct: false,
+      },
+      {
+        text: 'A5',
+        correct: true,
+      },
+      {
+        text: 'A8',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    subject: SubjectType.KAB,
+    question: 'Operace SubBytes plní v algoritmu AES stejnou roli, jako blok <ZVOLTE> v algoritmu DES',
+    img: null,
+    options: [
+      {
+        text: 'X-box',
+        correct: false,
+      },
+      {
+        text: 'P-box',
+        correct: false,
+      },
+      {
+        text: 'IN-box',
+        correct: false,
+      },
+      {
+        text: 'K-box',
+        correct: false,
+      },
+      {
+        text: 'E-box',
+        correct: false,
+      },
+      {
+        text: 'S-box',
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 15,
+    subject: SubjectType.KAB,
+    question: 'Šifrování symetrickým algoritmem je v porovnání se asymetrickým:',
+    img: null,
+    options: [
+      {
+        text: 'srovnatelně rychlé',
+        correct: false,
+      },
+      {
+        text: 'přibližně 100x rychlejší',
+        correct: true,
+      },
+      {
+        text: 'Asymetrické a sym. kryptosystémy nelze z tohoto hlediska srovnávat',
+        correct: false,
+      },
+      {
+        text: 'přibližně dvakrát rychlejší',
+        correct: false,
+      },
+      {
+        text: 'přibližně 100x pomalejší',
+        correct: false,
+      },
+      {
+        text: 'přibližně dvakrát pomalejší',
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 16,
+    subject: SubjectType.KAB,
+    question: 'Vernamova šifra',
+    img: null,
+    options: [
+      {
+        text: 'je výpočetně bezpečná',
+        correct: false,
+      },
+      {
+        text: 'se používá v asymetrické kryptografii',
+        correct: false,
+      },
+      {
+        text: 'byla prolomena v roce 1917',
+        correct: false,
+      },
+      {
+        text: 'je nepodmíněně bezpečná',
+        correct: true,
+      },
+      {
+        text: 'je prolomitelná pouze kvantovým počítačem',
+        correct: false,
+      },
+      {
+        text: 'je dodnes bezpečná',
         correct: true,
       },
     ],
